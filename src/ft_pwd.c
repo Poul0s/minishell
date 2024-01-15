7c587a835b7775c08fd81eb5da163565d09532d0
Barnabe Bonnet <babonnet@3A5.42angouleme.fr>
2024-01-15 15:21:24 +0100

    add get_path

--- src/ft_pwd.c
+++ src/ft_pwd.c
@@ -6,18 +6,19 @@
 /*   By: babonnet <babonnet@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 14:39:42 by babonnet          #+#    #+#             */
-/*   Updated: 2024/01/15 14:40:31 by babonnet         ###   ########.fr       */
+/*   Updated: 2024/01/15 15:21:06 by babonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-char *ft_pwd(void)
+char	*ft_pwd(void)
 {
 	static int	buffer_size = 1;
-	char			*res;
+	char		*res;
 	char		*buff;
 
 	buff = malloc(buffer_size);
@@ -34,4 +35,23 @@ char *ft_pwd(void)
 		res = getcwd(buff, buffer_size);
 	}
 	return (buff);
+}
+
+char	*get_path(void)
+{
+	char	*pass;
+	char	*home;
+	char	*path;
+
+	pass = ft_pwd();
+	if (!pass)
+		return (NULL);
+	home = getenv("HOME");
+	if (home)
+	{
+		path = strdup(pass + strlen(home) - 1);
+		path[0] = '~';
+	}
+	free(pass);
+	return (path);
 }
