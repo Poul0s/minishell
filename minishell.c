1e85a51aeed85ffe89bd4a59c69b16b7000d0a91
Paul Salame <psalame@1C2.42angouleme.fr>
2024-01-16 15:19:10 +0100

    added ctrl d -> exit

--- minishell.c
+++ minishell.c
@@ -6,10 +6,10 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 12:18:21 by psalame           #+#    #+#             */
-/*   Updated: 2024/01/16 03:08:41 by babonnet         ###   ########.fr       */
-/*   Updated: 2024/01/15 20:25:04 by psalame          ###   ########.fr       */
+/*   Updated: 2024/01/16 14:49:57 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 #include <readline/readline.h>
@@ -35,17 +35,17 @@ char *autocompletion(const char *str, int status)
 	return ((char *)head->content);
 }
 
-//static void	execute_line(char *command_line_str)
-//{
-//	t_command_line	*command_line;
-//
-//	// todo history fct
-//	command_line = parse_cmd_line(command_line_str);
-//	if (command_line)
-//		print_command_line(command_line, 0);
-//		// todo execute_command_line(command_line);
-//	// todo free command_line
-//}
+static void	execute_line(char *command_line_str)
+{
+	t_command_group	*command_line;
+
+	// todo history fct
+	command_line = parse_cmd_line(command_line_str);
+	if (command_line)
+		print_command_line(command_line, 0);
+		// todo execute_command_line(command_line);
+	// todo free command_line
+}
 
 int	main(void)
 {
@@ -54,13 +54,18 @@ int	main(void)
 
 	shell_data.hostname = get_hostname();
 	refresh_prompt(&shell_data);
-	line_readed = readline(shell_data.prompt);
-	while (line_readed != NULL)
+	line_readed = NULL;
+	while (1)
 	{
 		rl_completion_entry_function = autocompletion;
 		if (line_readed && *line_readed)
-			add_history (line_readed);
+			add_history(line_readed);
 		line_readed = readline(shell_data.prompt);
+		if (!line_readed)
+			break ;
+		else
+			execute_line(line_readed);
 	}
+	ft_dprintf(0, "exit\n");
 	return (0);
 }
