18aec902e0b953650726769813d809223e86d8aa
Paul Salame <psalame@3F4.42angouleme.fr>
2024-01-22 13:15:59 +0100

    readded wait pid for case of sleep in pipe (ex: (sleep 5 && echo abc) | cmdNotFound)

--- src/execution/execute_command_line.c
+++ src/execution/execute_command_line.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:02:32 by psalame           #+#    #+#             */
-/*   Updated: 2024/01/21 21:11:34 by psalame          ###   ########.fr       */
+/*   Updated: 2024/01/22 13:14:44 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,10 @@ int count_pipe(t_command_group *command_line)
 
 int	execute_command_line(t_command_group *command_line)
 {
-	t_pipe data_pipe;
-	int *pid;
-	int	last_pid_res;
+	t_pipe	data_pipe;
+	int		*pid;
+	int		last_pid_res;
+	int		i;
 
 	data_pipe.index = 0;
 	data_pipe.pipe_count = count_pipe(command_line);
@@ -42,5 +43,10 @@ int	execute_command_line(t_command_group *command_line)
 	if (waitpid(pid[data_pipe.pipe_count - 1], &last_pid_res, 0) == -1)
 		return (-1);
 	else
+	{
+		i = 0;
+		while (i < data_pipe.pipe_count - 1)
+			waitpid(pid[i++], NULL, 0);
 		return (WEXITSTATUS(last_pid_res));
+	}
 }
