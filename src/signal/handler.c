2055f22bf943af561626fa4858b521f955ff5478
Paul Salame <psalame@3G3.42angouleme.fr>
2024-01-26 17:08:37 +0100

    FIX FUCKING CTRL+\ YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- src/signal/handler.c
+++ src/signal/handler.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:38:09 by psalame           #+#    #+#             */
-/*   Updated: 2024/01/26 01:17:23 by psalame          ###   ########.fr       */
+/*   Updated: 2024/01/26 17:05:10 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,13 @@ static void	signal_handler(int signal)
 	}
 }
 
-static void	test(int signal)
+static void	ign(int signal)
 {
 	(void) signal;
-
-	if (signal == SIGQUIT)
-	{
-		exit_status = 131;
-		ft_dprintf(2, "Quit (core dumped)\n");
-	}
-	else
-	{
-		exit_status = 130;
-		ft_dprintf(2, "\n");
-	}
 }
 
 void	toggle_signal_handler(bool toggle)
 {
-	static struct sigaction	action;
-	sigset_t			mask;
 	struct termios		term_data;
 
 	tcgetattr(0, &term_data);
@@ -56,22 +43,14 @@ void	toggle_signal_handler(bool toggle)
 	{
 		term_data.c_lflag = term_data.c_lflag & (~ECHOCTL);
 		tcsetattr(0, 0, &term_data);
-		sigemptyset(&mask);
-		sigaddset(&mask, SIGQUIT);
-		sigaddset(&mask, SIGINT);
-		action.sa_mask = mask;
-		action.sa_flags = SA_NODEFER;
-		action.sa_handler = &signal_handler;
-		sigaction(SIGINT, &action, NULL);
-		sigaction(SIGQUIT, &action, NULL);
+		signal(SIGINT, signal_handler);
+		signal(SIGQUIT, signal_handler);
 	}
 	else
 	{
 		term_data.c_lflag = term_data.c_lflag | ECHOCTL;
-		action.sa_flags = SA_NODEFER;
 		tcsetattr(0, 0, &term_data);
-		action.sa_handler = &test;
-		sigaction(SIGINT, &action, NULL);
-		sigaction(SIGQUIT, &action, NULL);
+		signal(SIGINT, ign);
+		signal(SIGQUIT, ign);
 	}
 }
