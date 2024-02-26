c2f84e9b76bf63fa12dc00b8f5a28393ce587ef9
Paul Salame <psalame@2B1.42angouleme.fr>
2024-02-26 14:36:25 +0100

    fixed unclosed fd at get hostname

--- src/get/get_hostname.c
+++ src/get/get_hostname.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_hostname.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
+/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 14:15:14 by psalame           #+#    #+#             */
-/*   Updated: 2024/01/27 09:10:25 by psalame          ###   ########.fr       */
+/*   Updated: 2024/02/26 14:35:59 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,6 @@ char	*get_hostname(void)
 	hostname_end = ft_strchr(hostname, '\n');
 	if (hostname_end != 0)
 		*hostname_end = 0;
+	close(fd);
 	return (hostname);
 }
