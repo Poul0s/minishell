6ba0bc0d2959735686697014bbcbd8c829c1c2a2
Paul Salame <psalame@2B1.42angouleme.fr>
2024-02-26 14:15:19 +0100

    execute command line: normed + added malloc protection

--- src/execution/execute_command_line.c
+++ src/execution/execute_command_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_command_line.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: babonnet <babonnet@42angouleme.fr>         +#+  +:+       +#+        */
+/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 13:02:32 by psalame           #+#    #+#             */
-/*   Updated: 2024/02/23 23:02:28 by babonnet         ###   ########.fr       */
+/*   Updated: 2024/02/26 14:14:53 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,40 @@ int	count_pipe(t_command_group *command_line)
 	return (i);
 }
 
+static int	execute_piped_cmd_line(t_command_group *command_line,
+									t_execution_data exec_data)
+{
+	t_pipe	data_pipe;
+	int		last_pid_res;
+	int		i;
+
+	data_pipe.index = 0;
+	data_pipe.pipe_count = count_pipe(command_line);
+	exec_data.pid = malloc(data_pipe.pipe_count * sizeof(int));
+	if (exec_data.pid == NULL)
+	{
+		ft_dprintf(2, "%s: %s\n", exec_data.shell_data->exec_name,
+			strerror(ENOMEM));
+		return (ENOMEM);
+	}
+	pipe_cmd(command_line, exec_data, &data_pipe);
+	i = -1;
+	while (++i < data_pipe.pipe_count)
+	{
+		if (exec_data.pid[i] > 0)
+			waitpid(exec_data.pid[i], &last_pid_res, 0);
+	}
+	if (exec_data.pid[data_pipe.pipe_count - 1] <= 0)
+		last_pid_res = (-exec_data.pid[data_pipe.pipe_count - 1] - 1) << 8;
+	last_pid_res = WEXITSTATUS(last_pid_res);
+	free(exec_data.pid);
+	return (last_pid_res);
+}
+
 int	execute_command_line(t_command_group *command_line,
 		t_execution_data exec_data)
 {
-	t_pipe	data_pipe;
-	int		*pid;
 	int		last_pid_res;
-	int		i;
 	bool	forked;
 
 	forked = exec_data.forked;
@@ -48,24 +75,7 @@ int	execute_command_line(t_command_group *command_line,
 				NULL);
 	}
 	else
-	{
-		data_pipe.index = 0;
-		data_pipe.pipe_count = count_pipe(command_line);
-		pid = malloc(data_pipe.pipe_count * sizeof(int));
-		exec_data.pid = pid;
-		pipe_cmd(command_line, exec_data, &data_pipe);
-		i = 0;
-		while (i < data_pipe.pipe_count)
-		{
-			if (pid[i] > 0)
-				waitpid(pid[i], &last_pid_res, 0);
-			i++;
-		}
-		if (pid[data_pipe.pipe_count - 1] <= 0)
-			last_pid_res = (-pid[data_pipe.pipe_count - 1] - 1) << 8;
-		last_pid_res = WEXITSTATUS(last_pid_res);
-		free(pid);
-	}
+		last_pid_res = execute_piped_cmd_line(command_line, exec_data);
 	while (command_line->pipe_next)
 		command_line = command_line->pipe_next;
 	exec_data.forked = forked;
