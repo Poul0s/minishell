7c275629f9ffea4399e428864c2fda6c9ee1cf7e
Barnabe Bonnet <babonnet@2C7.42angouleme.fr>
2024-02-27 15:44:05 +0100

    mormed

--- include/here_doc.h
+++ include/here_doc.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
+/*   By: babonnet <babonnet@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 18:32:53 by babonnet          #+#    #+#             */
-/*   Updated: 2024/02/26 21:39:32 by psalame          ###   ########.fr       */
+/*   Updated: 2024/02/27 15:43:47 by babonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HERE_DOC_H
 # define HERE_DOC_H
-# include "minishell.h"
 # include "command.h"
+# include "minishell.h"
 
 typedef struct s_hd_reader_data
 {
@@ -22,21 +22,19 @@ typedef struct s_hd_reader_data
 	int					fd;
 	char				*file_name;
 	t_execution_data	exec_data;
-}	t_hd_reader_data;
+}						t_hd_reader_data;
 
-void	manage_here_doc(t_command_group *g_cmd, 
-						int *error,
-						t_execution_data exec_data);
-void	close_all_fd(t_command_group *g_cmd);
+void					manage_here_doc(t_command_group *g_cmd, int *error,
+							t_execution_data exec_data);
+void					close_all_fd(t_command_group *g_cmd);
 
-char	*create_tmp_file(int *fd);
-void	add_here_doc_data(t_infile *infile, int fd_file, char *file_name);
+char					*create_tmp_file(int *fd);
+void					add_here_doc_data(t_infile *infile, int fd_file,
+							char *file_name);
 
-void	pop_infile(void *content);
+void					pop_infile(void *content);
 
-int	read_here_doc(char *delimiter,
-				int fd,
-				char *file_name,
-				t_execution_data exec_data);
+int						read_here_doc(char *delimiter, int fd, char *file_name,
+							t_execution_data exec_data);
 
 #endif
